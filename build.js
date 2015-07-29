3e9c778851616c15b0c49e2412223c6ebab8ef3c
Matthew Bauer <mbauer@lexmark.com>
2015-07-29 17:48:16 -0500

    buildSFX doesn't let dynamic imports.

--- build.js
+++ build.js
@@ -3,9 +3,8 @@ var Builder = require('systemjs-builder')
 var builder = new Builder()
 builder.loadConfig('config.js').then(function () {
 	builder.config({defaultJSExtensions: true})
-  return builder.buildSFX('index.coffee!', 'index.min.js', {
-    minify: true,
-    runtime: false
+  return builder.build('index.coffee!', 'index.min.js', {
+    minify: true
   })
 })
 .then(function () {
