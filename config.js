667af41206bbdbf54f6e9c83e661eb6a48a5bddb
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-08-24 15:59:39 -0500

    Use bundling instead of jspm_packages.

--- config.js
+++ config.js
@@ -1,56 +1,82 @@
 System.config({
-  "transpiler": "traceur",
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   },
-  "bundles": {
-    "build": [
-      "github:satazor/sparkmd5@1.0.0/spark-md5",
-      "github:stuk/jszip@2.5.0/dist/jszip",
-      "github:matthewbauer/keypad@0.0.2/index",
-      "github:mohayonao/web-audio-api-shim@0.3.0/build/web-audio-api-shim",
-      "github:webcomponents/webcomponentsjs@0.7.10/webcomponents-lite",
-      "player.coffee!github:forresto/system-coffee@0.1.2",
-      "github:satazor/sparkmd5@1.0.0",
+  bundles: {
+    "build.js": [
+      "index.coffee!github:forresto/system-coffee@0.1.2",
       "github:stuk/jszip@2.5.0",
+      "github:satazor/sparkmd5@1.0.0",
       "github:matthewbauer/keypad@0.0.2",
-      "github:mohayonao/web-audio-api-shim@0.3.0",
-      "github:webcomponents/webcomponentsjs@0.7.10",
-      "github:matthewbauer/window@0.0.3/window",
-      "github:matthewbauer/document@0.0.4/document",
+      "x-retro.js",
+      "github:matthewbauer/keypad@0.0.2/index",
       "github:matthewbauer/window@0.0.3",
       "github:matthewbauer/document@0.0.4",
-      "x-retro",
-      "index.coffee!github:forresto/system-coffee@0.1.2"
+      "player.coffee!github:forresto/system-coffee@0.1.2",
+      "github:stuk/jszip@2.5.0/dist/jszip",
+      "github:satazor/sparkmd5@1.0.0/spark-md5",
+      "github:matthewbauer/document@0.0.4/document",
+      "github:matthewbauer/window@0.0.3/window",
+      "github:webcomponents/webcomponentsjs@0.7.12",
+      "github:mohayonao/web-audio-api-shim@0.3.0",
+      "github:mohayonao/web-audio-api-shim@0.3.0/build/web-audio-api-shim",
+      "github:webcomponents/webcomponentsjs@0.7.12/webcomponents-lite"
+    ],
+    "build-snes9x-next.js": [
+      "npm:snes9x-next@0.4.6",
+      "npm:snes9x-next@0.4.6/retro",
+      "npm:snes9x-next@0.4.6/core"
+    ],
+    "build-vba-next.js": [
+      "npm:vba-next@0.4.6",
+      "npm:vba-next@0.4.6/retro",
+      "npm:vba-next@0.4.6/core"
+    ],
+    "build-nestopia.js": [
+      "npm:nestopia@0.4.6",
+      "npm:nestopia@0.4.6/retro",
+      "npm:nestopia@0.4.6/core"
+    ],
+    "build-vecx.js": [
+      "npm:vecx@0.4.6",
+      "npm:vecx@0.4.6/retro",
+      "npm:vecx@0.4.6/core"
+    ],
+    "build-gambatte.js": [
+      "npm:gambatte@0.4.6",
+      "npm:gambatte@0.4.6/retro",
+      "npm:gambatte@0.4.6/core"
+    ],
+    "build-gw.js": [
+      "npm:gw@0.4.6",
+      "npm:gw@0.4.6/retro",
+      "npm:gw@0.4.6/core"
     ]
-  }
-});
+  },
 
-System.config({
-  "map": {
+  map: {
     "coffee": "github:forresto/system-coffee@0.1.2",
     "document": "github:matthewbauer/document@0.0.4",
     "gambatte": "npm:gambatte@0.4.6",
     "gw": "npm:gw@0.4.6",
     "jszip": "github:stuk/jszip@2.5.0",
     "keypad": "github:matthewbauer/keypad@0.0.2",
-    "localforage": "github:mozilla/localforage@master",
     "nestopia": "npm:nestopia@0.4.6",
     "snes9x-next": "npm:snes9x-next@0.4.6",
     "sparkmd5": "github:satazor/sparkmd5@1.0.0",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
     "vba-next": "npm:vba-next@0.4.6",
     "vecx": "npm:vecx@0.4.6",
     "window": "github:matthewbauer/window@0.0.3",
     "github:matthewbauer/document@0.0.4": {
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.10"
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.12"
     },
     "github:matthewbauer/window@0.0.3": {
       "web-audio-api-shim": "github:mohayonao/web-audio-api-shim@0.3.0"
     }
   }
 });
-
--- test.js
+++ test.js
@@ -3,6 +3,9 @@ var test = require('selenium-webdriver/testing')
 
 test.describe('Open app', function () {
   test.it('should work', function () {
+    browser
+      .get("http://nodejs.org/")
+      .nodeify(done);
     var driver = new webdriver.Builder().forBrowser('firefox').build()
     driver.quit()
   })
