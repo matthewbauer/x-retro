290722c980fdcbd1f6955361ccd79b54fcc4ffec
Paul DeCarlo <pdecarlo@microsoft.com>
2017-08-01 11:06:42 -0500

    Fix Heap out of Memory Error when running jspm bundle
    See:
    https://stackoverflow.com/questions/38558989/node-js-heap-out-of-memory

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jspm bundle --minify --skip-source-maps x-retro + raw + snes9x-next bundle.js
+node --max_old_space_size=4096 ./node_modules/jspm/jspm.js bundle --minify --skip-source-maps x-retro + raw + snes9x-next + gambatte + vba-next + nestopia + gw + vecx bundle.js
 cat jspm_packages/system.js bundle.js config.js > build.js
 rm bundle.js
 echo 'System.import("x-retro");' >> build.js
